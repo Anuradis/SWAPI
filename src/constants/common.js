d84bea2d6427f2f65982ff25c0329301c547fa89
Piotr Pachocki <piotr.pachocki@l7informatics.com>
2024-02-28 13:55:18 +0100

    common constants defined

---
+++ src/constants/common.js
@@ -0,0 +1,19 @@
+export const DISCARDED_FIRST_LETTER_LIST = Object.freeze(['L', 'N', 'C'])
+
+export const GENDER = Object.freeze({
+  male: 'male',
+  female: 'female'
+})
+
+export const BIRTH_YEAR_REQUIRED_FOR_FILL = '19BBY'
+
+export const INITIAL_PERSON_DETAILS = Object.freeze({
+  name: '',
+  height: '',
+  mass: '',
+  hair_color: '',
+  skin_color: '',
+  eye_color: '',
+  birth_year: '',
+  gender: ''
+})
