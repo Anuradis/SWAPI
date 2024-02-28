2fc5a8223f95c885fe62e22bfe3dc4fea0df6bef
Piotr Pachocki <piotr.pachocki@l7informatics.com>
2024-02-28 14:05:54 +0100

    clean up changes

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
-<script setup>
-</script>
-
 <template>
-  <main>
-    <Home />
+  <main class="main-view">
+    <router-view></router-view>
   </main>
 </template>
 
-<style scoped lang="scss">
+<style lang="scss" scoped>
+.main-view {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 </style>
