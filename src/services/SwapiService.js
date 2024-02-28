a405f98164aa085c15e38a8d9a33762ff671c69f
Piotr Pachocki <piotr.pachocki@l7informatics.com>
2024-02-28 13:57:15 +0100

    initial swapi service created

--- src/services/SwapiService.js
+++ src/services/SwapiService.js
@@ -1,4 +1,21 @@
 export default {
-  get
+  fetchPeopleDataByPage: async (page) => {
+    try {
+      const response = await fetch(`https://swapi.dev/api/people/?page=${page}`)
+      const data = await response.json()
+      return data
+    } catch (error) {
+      console.error('Error fetching people data:', error)
+    }
+  },
 
+  fetchPersonDetailsById: async (personId) => {
+    try {
+      const response = await fetch(`https://swapi.dev/api/people/${personId}/`)
+      const data = await response.json()
+      return data
+    } catch (error) {
+      console.error('Error fetching person data:', error)
+    }
+  }
 }
